72f6a0f04fc0599c01ccb3d83618771088ce9ad0
sunf044 <60446204+sunf044@users.noreply.github.com>
2024-07-23 11:50:57 +0700

    เรียน function

---
+++ ep2/index1.ts
@@ -0,0 +1,13 @@
+function g(homework: number, midterm: number, final: number) {
+    let score: number = homework + midterm + final
+
+    if (score < 50) {
+        return 'เกรด F'
+    } else if (score < 60){
+        return 'เกรด D'
+    } else if (score < 70){
+        return 'เกรด C'
+    }
+}
+
+console.log(g(10, 20, 60))
